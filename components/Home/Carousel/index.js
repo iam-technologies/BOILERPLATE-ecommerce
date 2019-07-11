9622b794e5b9b8dafd0336a2033555d3e75be61c
Ezequiel Burgos Lombardi <ezequielburgoslombardi@Ezequiels-MBP.home>
2019-07-11 16:49:34 +0200

    removing comments

--- components/Home/Carousel/index.js
+++ components/Home/Carousel/index.js
@@ -28,9 +28,7 @@ const Carousel = ({ items }) => {
         {
           (link && btnText)
           && (
-          <Link
-            to={link}
-          >
+          <Link to={link}>
             <a className="carousel_btn">{btnText}</a>
           </Link>
           )
@@ -60,62 +58,3 @@ const Carousel = ({ items }) => {
 };
 
 export default Carousel;
-
-// export default class Carousel extends Component {
-//   getSlide(elem) {
-//     const imgUrl = _get(elem, 'imgUrl', '');
-//     const btnText = _get(elem, 'btnText.es', '');
-//     const linkUrl = _get(elem, 'linkUrl', '');
-
-//     const link = linkUrl ? urlUtils.urlToPathLink(linkUrl) : '';
-
-//     if (!imgUrl) return null;
-//     const imgPath = imgUrl.split('.');
-//     const imgFormat = imgPath.pop();
-//     const imgFallback = `${imgPath.join('.')}.jpg`;
-
-//     return (
-//       <div className="carousel_home">
-//         <picture>
-//           {imgFormat == 'webp' && <source srcSet={imgServices.getUrl(imgUrl)} type="image/webp" />}
-//           <img src={imgServices.getUrl(imgFallback)} alt={imgUrl} />
-//         </picture>
-//         {
-//           (link && btnText)
-//           && (
-//           <Link
-//             to={link}
-//           >
-//             <a className="carousel_btn">
-//               {btnText}
-//             </a>
-//           </Link>
-//           )
-//         }
-//       </div>
-//     );
-//   }
-
-//   render() {
-//     const { items } = this.props;
-
-//     if (items.length < 1) return null;
-
-//     const images = items.map(elem => ({ renderItem: this.getSlide.bind(this, elem) }));
-
-//     return (
-//       <ImageGallery
-//         autoPlay
-//         disableArrowKeys
-//         items={images}
-//         showFullscreenButton={false}
-//         showNav={false}
-//         showPlayButton={false}
-//         showThumbnails={false}
-//         slideDuration={1200}
-//         slideInterval={6000}
-//         useTranslate3D={false}
-//       />
-//     );
-//   }
-// }
