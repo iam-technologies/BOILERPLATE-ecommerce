894e782c9829647b15a77505fbbbb1bcc93da993
Ezequiel Burgos Lombardi <ezequielburgoslombardi@Ezequiels-MBP.home>
2019-07-16 10:07:59 +0200

    saving budget page

--- pages/budget/[price].js
+++ pages/budget/[price].js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import { Layout, SearchByBudget } from '../../components';
 
 const searchByBudget = ({ price }) => (
@@ -8,12 +7,6 @@ const searchByBudget = ({ price }) => (
     <h1>this the budget page: {price}</h1>
     <SearchByBudget price={price} />
   </Layout>
-=======
-const searchByBudget = () => (
-  <div>
-    <h1>this is PRICE page</h1>
-  </div>
->>>>>>> 04765b9aec78f2498c6c2d4859898735a9a1bf93
 );
 
 searchByBudget.getInitialProps = async ({ query }) => {
